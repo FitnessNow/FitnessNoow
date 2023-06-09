9dac85e86f7f5a95b407804f852fab30fcbeceb1
Lucas Silva <gaviao.lucas@gmail.com>
2023-06-09 17:55:35 +0100

    small update

--- models/Exercise.model.js
+++ models/Exercise.model.js
@@ -3,14 +3,7 @@ const {mongoose,Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const exerciseSchema = new Schema(
   {
-    category: {
-      type: String,
-      
-    },
-    objective: {
-      type: String,
-      
-    }
+    
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
