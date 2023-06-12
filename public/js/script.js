ffc838f6512278c07c72f6b099ffa7f3628c4de8
Lucas Silva <gaviao.lucas@gmail.com>
2023-06-12 23:43:58 +0100

    added animation to the welcome text/home page

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("fitness-nowt JS imported successfully!");
 });
+
+const welcomeEl = document.getElementsByClassName("welcome");
+
+setTimeout(() => {
+  welcomeEl.style.display = block;
+}, 100)
