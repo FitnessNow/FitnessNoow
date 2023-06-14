9526d675c46a912fa3e7597e351454c19d58542b
Lucas Silva <gaviao.lucas@gmail.com>
2023-06-14 15:19:55 +0100

    added more style to login and signup and added login auth to balance

--- routes/balance.routes.js
+++ routes/balance.routes.js
@@ -7,7 +7,7 @@ const Income = require('../models/Income.model');
 const Expense = require('../models/Expense.model');
 const { calculateBalance } = require('../balance/balance');
 
-router.get("/balance", (req, res, next) => {
+router.get("/balance", isLoggedIn, (req, res, next) => {
   calculateBalance(req.session.currentUser._id)
       .then((balance) => {
           res.render("balance/balance-user", { balance });
